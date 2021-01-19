2165c92c4dd395d45687f31399f36e1390c51eae
Hasnain Raza <hasnainraza1072@gmail.com>
2021-01-20 01:03:28 +0530

    Added gradient type colour change to recycleview elements

--- app/src/main/java/com/example/passwordmanager/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/passwordmanager/RecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package com.example.passwordmanager;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +10,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private final Context context;
     private final ArrayList<String> arrayList;
+    int r = 30;
+    int g = 225;
+    int b = 30;
+    int shader = 5;
 
     public RecyclerViewAdapter(Context context, ArrayList<String> arrayList) {
         this.context = context;
@@ -33,7 +40,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.passwordData.setText(arrayList.get(position));
-
+        holder.cardView.setBackgroundColor(Color.rgb(r, g, b));
+        r += shader;
+        g += shader;
+        b += shader;
+        if (g > 235) {
+            r = 30;
+            g = 225;
+            b = 30;
+        }
     }
 
     @Override
@@ -43,13 +58,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public TextView passwordData;
+        Random random = new Random();
+        private TextView passwordData;
+        private CardView cardView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
 
             passwordData = itemView.findViewById(R.id.passwordData);
+            cardView = itemView.findViewById(R.id.cardview);
         }
 
         @Override
@@ -60,11 +78,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                     .setMessage("Do you really want to delete this entry ?")
                     .setPositiveButton("Yes", (dialog, which) -> {
                         int position = this.getAdapterPosition();
-                        String[]strings = arrayList.get(position).split("\n");
+                        String[] strings = arrayList.get(position).split("\n");
                         arrayList.remove(position);
                         notifyDataSetChanged();
                         Toast.makeText(context, "Entry deleted!", Toast.LENGTH_SHORT).show();
-                        new PasswordDatabaseHelper(context).deleteData(strings[0],strings[1],strings[2]);
+                        new PasswordDatabaseHelper(context).deleteData(strings[0], strings[1], strings[2]);
                     })
                     .setNegativeButton("No", null)
                     .show();
